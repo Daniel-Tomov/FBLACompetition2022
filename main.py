5630957bce679de829ede6a927435b891161360c
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-02-21 23:05:31 +0000

    add more locations and map test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import sys
+
 sys.path.append(".")
 from Class import *
 from attractions import storedAttr, attr
@@ -15,103 +16,122 @@ returnList = []
 filters = []
 provinceList = []
 
+
 def remove_filter_from_list(val, actualVal):
-  global provinceList
-  global filters
-  global checkList
-  #check if the filter was checked
-  if val == "on":
-    #set the same index as the filter in its list in the checked list
-    checkList[filters.index(actualVal)] = "checked"
-    for i in storedAttr:
-      arr = i.get_data()
-      #remove attractions that do not meet the filter
-      if actualVal not in arr[2]:
-        if arr in returnList:
-          returnList.remove(arr)
-    
+    global provinceList
+    global filters
+    global checkList
+    #check if the filter was checked
+    if val == "on":
+        #set the same index as the filter in its list in the checked list
+        checkList[filters.index(actualVal)] = "checked"
+        for i in storedAttr:
+            arr = i.get_data()
+            #remove attractions that do not meet the filter
+            if actualVal not in arr[2]:
+                if arr in returnList:
+                    returnList.remove(arr)
+
 
 def remove_prov_from_list(val, actualVal):
-  global provinceList
-  global filters
-  global checkList
-  #check if the the user selected the province
-  if val == actualVal:
-    #set the same index as the province in its list in the checked list
-    checkList[provinceList.index(actualVal) + len(filters)] = "checked"
-    for i in storedAttr:
-      data = i.get_data()
-      #Splits the address into an array
-      prov = data[0].split(", ")
-      #remove attractions that are not in the selected province
-      if data[0] != "" and actualVal not in prov[1]:
-        if data in returnList:
-          returnList.remove(data)
+    global provinceList
+    global filters
+    global checkList
+    #check if the the user selected the province
+    if val == actualVal:
+        #set the same index as the province in its list in the checked list
+        checkList[provinceList.index(actualVal) + len(filters)] = "checked"
+        for i in storedAttr:
+            data = i.get_data()
+            #Splits the address into an array
+            prov = data[0].split(", ")
+            #remove attractions that are not in the selected province
+            if data[0] != "" and actualVal not in prov[1]:
+                if data in returnList:
+                    returnList.remove(data)
+
 
 def clear_checkList():
-  global checkList
-  global filters
-  global provinceList
-  checkList = []
-  for i in range(len(filters) + len(provinceList)):
-    checkList.append('')
+    global checkList
+    global filters
+    global provinceList
+    checkList = []
+    for i in range(len(filters) + len(provinceList)):
+        checkList.append('')
 
 
 def add_records_to_list():
-  global returnList
-  returnList = []
-  #adds attractions to the list so they can be removed later on
-  for i in storedAttr:
-    rec = i.get_data()
-    returnList.append(rec)
+    global returnList
+    returnList = []
+    #adds attractions to the list so they can be removed later on
+    for i in storedAttr:
+        rec = i.get_data()
+        returnList.append(rec)
 
+
+@app.route("/map", methods=["POST", "GET"])
+def map():
+    return render_template("other.html",
+                           key='AIzaSyCztFq5Me_V6Neh2RF9G0s9qoSQz9w2AvE')
 
 
 @app.route("/", methods=["POST", "GET"])
 def main():
-  #import global variables
-  global checkList
-  global returnList
+    #import global variables
+    global checkList
+    global returnList
 
-  #add the attractions to a list so they can be directly passed for a GET request
-  #or they can be removed for a POST request. The POST includes the filters the user selected
-  add_records_to_list()
+    #add the attractions to a list so they can be directly passed for a GET request
+    #or they can be removed for a POST request. The POST includes the filters the user selected
+    add_records_to_list()
 
-  #Run if the user is sending data back
-  if request.method == "POST":
-    #clears the checklist because we don't know what the user checked yet
-    clear_checkList()
-    #Perform the operations if the button with search was pressed
-    if (request.form['button'] == 'search'):
-      #remove attractions that don't fit the filters the user wanted
-      for f in filters:
-        remove_filter_from_list(request.form.get(f), f)
-      #remove provinces from the list that the user didn't want
-      for p in provinceList:
-        remove_prov_from_list(request.form.get("provinceSelector"), p)
-      
-      return render_template("index.html", attraction = returnList, provinceList = provinceList, filters = filters, checkBoxes = checkList)
+    #Run if the user is sending data back
+    if request.method == "POST":
+        #clears the checklist because we don't know what the user checked yet
+        clear_checkList()
+        #Perform the operations if the button with search was pressed
+        if (request.form['button'] == 'search'):
+            #remove attractions that don't fit the filters the user wanted
+            for f in filters:
+                remove_filter_from_list(request.form.get(f), f)
+            #remove provinces from the list that the user didn't want
+            for p in provinceList:
+                remove_prov_from_list(request.form.get("provinceSelector"), p)
 
-  #Runs if the user is not sending data back
-  elif request.method == "GET":
-    #clears the checklist because the user does not have anything checked
-    clear_checkList()
-    return render_template("index.html", attraction = attr, provinceList = provinceList, filters = filters, checkBoxes = checkList)
+            return render_template(
+                "index.html",
+                attraction=returnList,
+                provinceList=provinceList,
+                filters=filters,
+                checkBoxes=checkList)
+
+    #Runs if the user is not sending data back
+    elif request.method == "GET":
+        #clears the checklist because the user does not have anything checked
+        clear_checkList()
+        return render_template("index.html",
+                               attraction=attr,
+                               provinceList=provinceList,
+                               filters=filters,
+                               checkBoxes=checkList)
+
 
 #create the "fitler" and "ProvinceList" that will be passed to the html
 for i in range(0, len(storedAttr)):
-  indvAttr = storedAttr[i].get_data()
-  if "" != indvAttr[0]:
-    split = indvAttr[0].split(", ")
-    if split[1] not in provinceList:
-      provinceList.append(split[1])
-  if "" != indvAttr[2]:
-    split = indvAttr[2].split(", ")
-    for n in split:
-      if n not in filters:
-        filters.append(n)
+    indvAttr = storedAttr[i].get_data()
+    if "" != indvAttr[0]:
+        split = indvAttr[0].split(", ")
+        print(split)
+        if split[1] not in provinceList:
+            provinceList.append(split[1])
+    if "" != indvAttr[2]:
+        split = indvAttr[2].split(", ")
+        for n in split:
+            if n not in filters:
+                filters.append(n)
+#Sort the provinces in alphabetical order
 provinceList.sort()
 filters.sort()
-  
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
