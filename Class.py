1081c3f582b73d94a8ccfd8d12e79938f91cda2b
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-03-07 20:06:38 -0500

    copyright and fix class

--- Class.py
+++ Class.py
@@ -1,14 +1,14 @@
 class attractions:
-  def __init__(self, address, website, tags, image, name, long, lat):
+  def __init__(self, address, website, tags, image, name, longi, lat):
     self.address = address
     self.website = website
     self.tags = tags
     self.image = image
     self.name = name
-    self.long = long
+    self.longi = longi
     self.lat = lat
 
 
   def get_data(self):
-    return [self.address, self.website, self.tags, self.image, self.name, self.long, self.lat]
+    return [self.address, self.website, self.tags, self.image, self.name, self.longi, self.lat]
 
